b50103a03969bdb52d35a6c86d225a8103ed0c53
ajugeorge98 <54764698+ajugeorge98@users.noreply.github.com>
2023-02-14 03:59:18 -0800

    Add files via upload

--- compile.sh
+++ compile.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 # Some general variables
+PHONE="x3pro"
 ARCH="arm64"
 SUBARCH="arm64"
 DEFCONFIG=nogravity_defconfig
 COMPILER=clang
 LINKER="lld"
 COMPILERDIR="/media/pierre/Expension/Android/PocoX3Pro/Kernels/Proton-Clang"
+
+# Cleanup output
+rm -rf out/outputs/${PHONE}/*
 
 # Export shits
 export KBUILD_BUILD_USER=Pierre2324
@@ -48,8 +52,21 @@ OBJCOPY=llvm-objcopy \
 OBJDUMP=llvm-objdump \
 STRIP=llvm-strip \
 ld-name=${LINKER} \
-KBUILD_COMPILER_STRING="Proton Clang" \
+KBUILD_COMPILER_STRING="Proton-Clang" \
 Image.gz-dtb dtbo.img
+}
+
+miui_fix_dimens() {
+    sed -i 's/<70>/<695>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-36-02-0a-lcd-dsc-vid.dtsi
+    sed -i 's/<154>/<1546>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-36-02-0a-lcd-dsc-vid.dtsi
+    sed -i 's/<70>/<695>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-42-02-0b-lcd-dsc-vid.dtsi
+    sed -i 's/<154>/<1546>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-42-02-0b-lcd-dsc-vid.dtsi
+}
+restore_dimens() {
+    sed -i 's/<695>/<70>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-36-02-0a-lcd-dsc-vid.dtsi
+    sed -i 's/<1546>/<154>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-36-02-0a-lcd-dsc-vid.dtsi
+    sed -i 's/<695>/<70>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-42-02-0b-lcd-dsc-vid.dtsi
+    sed -i 's/<1546>/<154>/g' arch/arm64/boot/dts/qcom/dsi-panel-j20s-42-02-0b-lcd-dsc-vid.dtsi
 }
 
 # Make defconfig
@@ -75,6 +92,22 @@ then
     echo "Build failed"
 else
     echo "Build succesful"
+    mkdir out/outputs
+    mkdir out/outputs/${PHONE}
+    find out/arch/arm64/boot/dts/qcom/ -name '*.dtb' -exec cat {} + >out/outputs/${PHONE}/dtb
+    cp out/arch/arm64/boot/dtbo.img out/outputs/${PHONE}/dtbo.img
+    cp out/arch/arm64/boot/Image.gz out/outputs/${PHONE}/Image.gz
+    #MIUI dtbo
+    rm out/outputs/dtbo-miui.img
+    miui_fix_dimens
+    echo | Build_lld
+    if [ $? -ne 0 ]
+    then
+        rm out/outputs/dtbo-miui.img
+    else
+        cp out/arch/arm64/boot/dtbo.img out/outputs/dtbo-miui.img
+    fi
+    restore_dimens
 fi
 
 BUILD_END=$(date +"%s")
